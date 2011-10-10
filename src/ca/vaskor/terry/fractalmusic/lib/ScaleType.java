2a4937c6e84621cd4d0bec07dbe2e0851faa916a
Terry Vaskor <no@email.here>
2011-10-10 00:20:39 -0400

    Scale name correction
    Messiaen's scales should be modes of limited transposition, not
    transportation.
    Only the names as returned by toString() have been modified; the
    internal enum values still have misleading names.

--- src/ca/vaskor/terry/fractalmusic/lib/ScaleType.java
+++ src/ca/vaskor/terry/fractalmusic/lib/ScaleType.java
@@ -20,13 +20,13 @@ public enum ScaleType {
     HEXATONIC_BLUES("Hexatonic Blues", 3,2,1,1,3,2),
     HEPTATONIC_BLUES("Heptatonic Blues", 2,1,2,1,3,1,2),
     NINE_NOTE_BLUES("Nine Note Blues", 2,1,1,1,2,2,1,1,1),
-    LIMITED_TRANSPORTATION_1("Mode of Limited Transportation #1", 2,2,2,2,2,2),
-    LIMITED_TRANSPORTATION_2("Mode of Limited Transportation #2", 1,2,1,2,1,2,1,2),
-    LIMITED_TRANSPORTATION_3("Mode of Limited Transportation #3", 2,1,1,2,1,1,2,1,1),
-    LIMITED_TRANSPORTATION_4("Mode of Limited Transportation #4", 1,1,3,1,1,1,3,1),
-    LIMITED_TRANSPORTATION_5("Mode of Limited Transportation #5", 1,4,1,1,4,1),
-    LIMITED_TRANSPORTATION_6("Mode of Limited Transportation #6", 2,2,1,1,2,2,1,1),
-    LIMITED_TRANSPORTATION_7("Mode of Limited Transportation #7", 1,1,1,2,1,1,1,1,2,1);
+    LIMITED_TRANSPORTATION_1("Mode of Limited Transposition #1", 2,2,2,2,2,2),
+    LIMITED_TRANSPORTATION_2("Mode of Limited Transposition #2", 1,2,1,2,1,2,1,2),
+    LIMITED_TRANSPORTATION_3("Mode of Limited Transposition #3", 2,1,1,2,1,1,2,1,1),
+    LIMITED_TRANSPORTATION_4("Mode of Limited Transposition #4", 1,1,3,1,1,1,3,1),
+    LIMITED_TRANSPORTATION_5("Mode of Limited Transposition #5", 1,4,1,1,4,1),
+    LIMITED_TRANSPORTATION_6("Mode of Limited Transposition #6", 2,2,1,1,2,2,1,1),
+    LIMITED_TRANSPORTATION_7("Mode of Limited Transposition #7", 1,1,1,2,1,1,1,1,2,1);
     
     ScaleType(String name, int ... semitonesBetweenScaleDegrees) {
         scaleName = name;
